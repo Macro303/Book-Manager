6fcf87948b47964cf76fd3bcf0846147f07a9741
Macro303 <Macro303@users.noreply.github.com>
2019-11-12 20:55:53 +1300

    Add Wishlist list

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -30,42 +30,13 @@ function loadCollection() {
 			contentType: 'application/json'
 		},
 		dataType: 'json',
-		success: function(data){
-			for(let book of data){
-				let column = document.createElement('div');
-				column.className = 'column';
-				let segment = document.createElement('div');
-				segment.className = 'ui segment inverted';
-				let imageDiv = document.createElement('div');
-				imageDiv.className = 'ui small image';
-				let label = null;
-				if (book.Count !== 1) {
-					label = document.createElement('a');
-					label.className = 'ui small teal right ribbon label';
-					label.textContent = book.Count;
-				}
-				let image = document.createElement('img');
-				image.className = 'ui small image';
-				image.src = book.Book.Images.Medium;
-				let header = document.createElement('div');
-				header.className = 'ui center aligned small inverted header';
-				header.textContent = book.Book.Title;
-				let subheader = document.createElement('div');
-				subheader.className = 'sub header';
-				subheader.textContent = book.Book.Subtitle;
-
-				header.appendChild(subheader);
-				imageDiv.appendChild(image);
-				if (book.Count !== 1) {
-					imageDiv.appendChild(label);
-				}
-				segment.appendChild(imageDiv);
-				segment.appendChild(header);
-				column.appendChild(segment);
+		success: function (data) {
+			for (let book of data) {
+				let column = bookToHTML(book);
 				document.getElementById('collection-grid').appendChild(column);
 			}
 		},
-		error: function(xhr, status, error){
+		error: function (xhr, status, error) {
 			alert("#ERR: xhr.status=" + xhr.status + ", xhr.statusText=" + xhr.statusText + "\nstatus=" + status + ", error=" + error);
 		}
 	});
@@ -88,7 +59,7 @@ function addToCollection() {
 			document.getElementById("collectionAddButton").className = "ui inverted blue button";
 			location.reload();
 		},
-		error: function(xhr, status, error){
+		error: function (xhr, status, error) {
 			alert("#ERR: xhr.status=" + xhr.status + ", xhr.statusText=" + xhr.statusText + "\nstatus=" + status + ", error=" + error);
 			document.getElementById("collectionAddButton").className = "ui inverted blue button";
 			location.reload();
@@ -97,14 +68,84 @@ function addToCollection() {
 }
 
 function loadWishlist() {
-	for (var i = 0; i < 18; i++) {
-		var column = document.createElement('div');
-		column.className = 'column';
-		var segment = document.createElement('div');
-		segment.className = 'ui placeholder segment inverted';
-		column.appendChild(segment);
-		document.getElementById('wishlist-grid').appendChild(column)
+	$.ajax({
+		async: false,
+		url: '/api/books/wishlist',
+		type: 'GET',
+		headers: {
+			accept: 'application/json',
+			contentType: 'application/json'
+		},
+		dataType: 'json',
+		success: function (data) {
+			for (let book of data) {
+				let column = bookToHTML(book);
+				document.getElementById('wishlist-grid').appendChild(column);
+			}
+		},
+		error: function (xhr, status, error) {
+			alert("#ERR: xhr.status=" + xhr.status + ", xhr.statusText=" + xhr.statusText + "\nstatus=" + status + ", error=" + error);
+		}
+	});
+}
+
+function addToWishlist() {
+	document.getElementById("wishlistAddButton").className = "ui loading inverted blue button";
+	$.ajax({
+		async: true,
+		url: '/api/books/wishlist',
+		type: 'POST',
+		contentType: 'application/json',
+		data: JSON.stringify({
+			"isbn": document.getElementById('wishlist_isbn').value.toString(),
+			"format": document.getElementById('wishlist_format').value
+		}),
+		dataType: 'json',
+		success: function (data) {
+			console.log("Response: " + data);
+			document.getElementById("wishlistAddButton").className = "ui inverted blue button";
+			location.reload();
+		},
+		error: function (xhr, status, error) {
+			alert("#ERR: xhr.status=" + xhr.status + ", xhr.statusText=" + xhr.statusText + "\nstatus=" + status + ", error=" + error);
+			document.getElementById("wishlistAddButton").className = "ui inverted blue button";
+			location.reload();
+		}
+	});
+}
+
+function bookToHTML(book) {
+	let column = document.createElement('div');
+	column.className = 'column';
+	let segment = document.createElement('div');
+	segment.className = 'ui segment inverted';
+	let imageDiv = document.createElement('div');
+	imageDiv.className = 'ui small image';
+	let label = null;
+	if (book.Count !== 1) {
+		label = document.createElement('a');
+		label.className = 'ui small teal right ribbon label';
+		label.textContent = book.Count;
 	}
+	let image = document.createElement('img');
+	image.className = 'ui small image';
+	image.src = book.Book.Images.Medium;
+	let header = document.createElement('div');
+	header.className = 'ui center aligned small inverted header';
+	header.textContent = book.Book.Title;
+	let subheader = document.createElement('div');
+	subheader.className = 'sub header';
+	subheader.textContent = book.Book.Subtitle;
+
+	header.appendChild(subheader);
+	imageDiv.appendChild(image);
+	if (book.Count !== 1) {
+		imageDiv.appendChild(label);
+	}
+	segment.appendChild(imageDiv);
+	segment.appendChild(header);
+	column.appendChild(segment);
+	return column
 }
 
 function loadLoans() {
