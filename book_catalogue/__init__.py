27726e184ddbb6d4979ca5107214cb244f947ef3
Buried-In-Code <6057651+Buried-In-Code@users.noreply.github.com>
2022-11-29 11:02:57 +1300

    Add Settings (#70)
    - Updated dependencies

--- book_catalogue/__init__.py
+++ book_catalogue/__init__.py
@@ -1,8 +1,14 @@
-__version__ = "0.1.0b0"
-__all__ = ["__version__", "get_project_root"]
+__version__ = "0.1.0"
+__all__ = ["__version__", "get_project_root", "get_config_root"]
 
 from pathlib import Path
 
 
 def get_project_root() -> Path:
     return Path(__file__).parent.parent
+
+
+def get_config_root() -> Path:
+    root = Path.home() / ".config" / "book-catalogue"
+    root.mkdir(parents=True, exist_ok=True)
+    return root
--- run.py
+++ run.py
@@ -1,14 +1,22 @@
-from pathlib import Path
-
 import uvicorn
 
-if __name__ == "__main__":
-    Path("logs").mkdir(exist_ok=True)
+from book_catalogue import get_project_root
+from book_catalogue.settings import Settings
+
+
+def main():
+    settings = Settings.load().save()
+    log_folder = get_project_root() / "logs"
+    log_folder.mkdir(parents=True, exist_ok=True)
     uvicorn.run(
         "book_catalogue.__main__:app",
-        host="0.0.0.0",
-        port=8003,
+        host=settings.web.host,
+        port=settings.web.port,
         use_colors=True,
         server_header=False,
         log_config="log_config.yaml",
     )
+
+
+if __name__ == "__main__":
+    main()
